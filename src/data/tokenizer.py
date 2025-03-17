e05348fa0d17fc6fceafaa6ed833a1141ba77636
Shullpie <shullpie@gmail.com>
2025-03-17 14:46:53 +0300

    add get_pad_idx and length of vocab to Tokenizer

--- src/data/tokenizer.py
+++ src/data/tokenizer.py
@@ -11,6 +11,9 @@ class Tokenizer():
         self.tokenizer = self._get_tokenizer()
         logger.info('Tokenizer initialization completed.')
 
+    def __len__(self):
+        return len(self.tokenizer)
+
     def tokenize(self, text: str) -> list[str]:
         return self.tokenizer.tokenize(
             self.tokenizer.bos_token + text + self.tokenizer.eos_token
@@ -20,6 +23,12 @@ class Tokenizer():
         tokens = self.tokenize(text)
         return torch.LongTensor(self.tokenizer.convert_tokens_to_ids(tokens))
 
+    def get_pad_idx(self):
+        return self.tokenizer.convert_tokens_to_ids(self.tokenizer.pad_token)
+    
+
     @staticmethod
     def _get_tokenizer() -> AutoTokenizer:
        return AutoTokenizer.from_pretrained(config['data']['vocab_path'])
+    
+
